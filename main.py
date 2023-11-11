e1a2cd04b22e301e17d04c0684f94b1780401f7f
Sharat Chandra <sharatchandra897@gmail.com>
2023-11-11 13:23:25 +0530

    Blogs with query parameter

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 @app.get("/")
 def index():
     return{"Application" : "Create and Read Blogs"}
+
+@app.get("/blogsqueryparameter")
+def blogs_with_query_parameter(published: bool = True, limit: int = 25, sort: Optional[str] = None):
+    if(published):
+        return{"data": f'Published Posts {limit} are fetched {sort}'}
+    else:   
+        return{"data": f'Unpublished Posts {limit} are fetched {sort}'}
